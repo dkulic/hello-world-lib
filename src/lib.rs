2e54d365dab20ca8486724897daa17dbf4af22bd
Darko Kulic <darko.kulic@sicpa.com>
2023-05-31 09:41:48 +0200

    Initial version
    Signed-off-by: Darko Kulic <darko.kulic@sicpa.com>

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn hello(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn hello_works() {
+        let greet = hello("Peter");
+        assert_eq!(greet, "Hello Peter!")
+    }
+}

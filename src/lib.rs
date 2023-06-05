035c1b826fb69155ff60741dcc52d4e59d43d2a3
Darko Kulic <darko.kulic@sicpa.com>
2023-06-05 09:51:02 +0200

    Wasm code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[wasm_bindgen]
 pub fn hello(name: &str) -> String {
     format!("Hello {}!", name)
 }
